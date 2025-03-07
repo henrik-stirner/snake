43af3b4527a2f11946c6801e0fec841b288bfc14
Henrik Stirner <henrikstir@gmail.com>
2025-03-07 15:01:04 +0100

    auswertungsanzeige verbesser

--- win/tl/auswertungfenster.py
+++ win/tl/auswertungfenster.py
@@ -57,15 +57,26 @@ class AuswertungFenster(Nebenfenster):
         schlangen.sort(key=lambda obj: obj.laenge, reverse=True)
 
         # Anzeige
-        self.score_frame = Frame(self.frame)
-        self.score_frame.pack(pady=25, padx=25)
+        self.anzeige_frame = Frame(self.frame)
+        self.anzeige_frame.grid_columnconfigure(tuple(range(2)), weight=1)
+        self.anzeige_frame.pack(pady=25, padx=25)
+
+        self.spieler_frame = Frame(self.anzeige_frame)
+        self.spieler_frame.grid(row=0, column=0, sticky="news")
+        self.spieler_info_label = Label(self.spieler_frame, text="SPIELER")
+        self.spieler_info_label.pack(expand=True, fill=X, pady=10)
+
+        self.score_frame = Frame(self.anzeige_frame)
+        self.score_frame.grid(row=0, column=1, sticky="news")
+        self.score_info_label = Label(self.score_frame, text="SCORE")
+        self.score_info_label.pack(expand=True, fill=X, pady=10)
 
         farbe = lambda obj: "red" if obj.tot else "green" if obj.name == gewinner.name else "white"
         for obj in schlangen:
-            anzeigetext = f"Spieler: {obj.name}\t Score: {obj.laenge}"
-
-            self.score_label = Label(self.score_frame, foreground=farbe(obj), text=anzeigetext)
-            self.score_label.pack(expand=True, fill=X)
+            spieler_label = Label(self.spieler_frame, foreground=farbe(obj), text=obj.name)
+            spieler_label.pack(expand=True, fill=X)
+            score_label = Label(self.score_frame, text=str(obj.laenge))
+            score_label.pack(expand=True, fill=X)
 
         # Wiederholen-Knopf
         self.wiederholen_knopf = Button(self.frame, style="Big.TButton", text="ERNEUT SPIELEN", command=self.eingabe)
